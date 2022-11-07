c4b28c1314dd9ce57981836f45f2610a6462dbb8
Ssenkull <bogdan.zbarazhskiy@gmail.com>
2022-11-07 14:15:27 +0200

    added comic page and 404page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import {MainPage, ComicsPage} from '../pages';
+import {MainPage, ComicsPage, Page404, SingleComicPage} from '../pages';
 import AppHeader from "../appHeader/AppHeader";
 
 const App = () => {
@@ -13,7 +13,9 @@ const App = () => {
             <main>
                <Routes>
                 <Route  path="/" element={<MainPage/>}/>
-                    <Route  path = "/comics" element={<ComicsPage/>}/>
+                <Route  path = "/comics" element={<ComicsPage/>}/>
+                <Route  path = "/comics/:comicId" element={<SingleComicPage/>}/>
+                <Route path="*" element={<Page404/>}/>
                </Routes>
             </main>
         </div>
