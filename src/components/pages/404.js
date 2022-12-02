85b1a56c986dfec8bf7f58a0dfd35fb6d397e2bc
Ssenkull <bogdan.zbarazhskiy@gmail.com>
2022-12-02 11:01:05 +0200

    added search bar and also added seo optimization

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,12 +1,20 @@
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import {Link} from 'react-router-dom';
+import { Helmet } from "react-helmet";
 
 const Page404 = () => {
     return (
         <div>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="This page is not found"
+                />
+                <title>This page is not found</title>
+            </Helmet>
             <ErrorMessage/>
             <p style={{'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px'}}>Page doesn't exist</p>
-            <Link style={{'display': 'block', 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px', 'marginTop': '30px'}} to="/">Back to main page</Link>
+            <Link style={{'display': 'block', 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px', 'marginTop': '30px', 'color': '#9F0013'}} to="/">Back to main page</Link>
         </div>
     )
 }
