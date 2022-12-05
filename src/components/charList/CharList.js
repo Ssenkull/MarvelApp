24c4c6ef58031730814877538262dfc740c7e5bd
Ssenkull <bogdan.zbarazhskiy@gmail.com>
2022-12-05 08:05:51 +0200

    used fsm and fixed some bugs

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,10 +1,30 @@
-import {useState, useEffect, useRef} from 'react';
+import {useState, useEffect, useRef, useMemo} from 'react';
 import PropTypes from 'prop-types';
 
+import useMarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
-import useMarvelService from '../../services/MarvelService';
+
 import './charList.scss';
+
+const setContent = (process, Component, newItemLoading) =>  {
+    switch (process) {
+        case 'waiting':
+                return <Spinner/>;
+                break;
+        case 'loading':
+                return newItemLoading ? <Component/> :  <Spinner/>;
+                break;
+        case 'confirmed':
+                return <Component/>;
+                break;
+        case 'error':
+                return <ErrorMessage/>;
+                break;
+        default:
+                throw new Error('Unexpected process state');
+    }
+}
 
 const CharList = (props) => {
 
@@ -13,7 +33,7 @@ const CharList = (props) => {
     const [offset, setOffset] = useState(210);
     const [charEnded, setCharEnded] = useState(false);
 
-    const {loading, error, getAllCharacters} = useMarvelService();
+    const {getAllCharacters, process, setProcess} = useMarvelService();
 
     useEffect(() => {
         onRequest(offset, true);
@@ -23,6 +43,7 @@ const CharList = (props) => {
         initial ? setNewItemLoading(false) :  setNewItemLoading(true);
         getAllCharacters(offset)
             .then(onCharListLoaded)
+            .then(() => setProcess('confirmed'))
     }
 
 
@@ -91,17 +112,13 @@ const CharList = (props) => {
         )
     }
     
-    const items = renderItems(charList);
-
-    const errorMessage = error ? <ErrorMessage/> : null;
-    const spinner = loading && !newItemLoading ? <Spinner/> : null;
-   
+   const elements = useMemo(() => {
+    return setContent(process, () => renderItems(charList), newItemLoading);
+   }, [process])
 
     return (
         <div className="char__list">
-            {errorMessage}
-            {spinner}
-            {items}
+            {elements}
             <button 
                 className="button button__main button__long"
                 disabled={newItemLoading}
